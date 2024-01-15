78a9146d6147da928982bb1e24c7184e269003a3
sivaprakashkk-one <153369693+sivaprakashkk-one@users.noreply.github.com>
2024-01-15 23:40:15 +0530

    Optimize import

--- sm-core/src/main/java/com/salesmanager/core/business/modules/email/PostmarkEmailSenderImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/email/PostmarkEmailSenderImpl.java
@@ -11,11 +11,7 @@ import org.springframework.mail.MailPreparationException;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import javax.mail.BodyPart;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeBodyPart;
-import java.io.*;
-import java.util.HashMap;
+import java.io.StringWriter;
 import java.util.Map;
 
 @Component("postmarkEmailSender")
@@ -66,77 +62,6 @@ public class PostmarkEmailSenderImpl implements EmailModule {
         Message message = new Message("\"Tire wheel warehouse\" info@twwusa.store", to, subject, htmlWriter.toString(), textWriter.toString());
         message.setMessageStream("outbound");
         MessageResponse response = mailClient.deliverMessage(message);
-    }
-
-    private Map<String, String> constructHTMLMessage(String tmpl, Map<String, String> templateTokens) throws IOException, MessagingException {
-        // Create a "text" Multipart message
-        BodyPart textPart = new MimeBodyPart();
-        freemarkerMailConfiguration.setClassForTemplateLoading(DefaultEmailSenderImpl.class, "/");
-        Template textTemplate = freemarkerMailConfiguration.getTemplate(
-                TEMPLATE_PATH + "/" + tmpl);
-        final StringWriter textWriter = new StringWriter();
-        try {
-            textTemplate.process(templateTokens, textWriter);
-        } catch (TemplateException | IOException e) {
-            throw new MailPreparationException("Can't generate text mail", e);
-        }
-        textPart.setDataHandler(new javax.activation.DataHandler(new javax.activation.DataSource() {
-            public InputStream getInputStream() throws IOException {
-                // return new StringBufferInputStream(textWriter
-                // .toString());
-                return new ByteArrayInputStream(textWriter.toString().getBytes(CHARSET));
-            }
-
-            public OutputStream getOutputStream() throws IOException {
-                throw new IOException("Read-only data");
-            }
-
-            public String getContentType() {
-                return "text/plain";
-            }
-
-            public String getName() {
-                return "main";
-            }
-        }));
-
-
-        // Create a "HTML" Multipart message
-//        Multipart htmlContent = new MimeMultipart("related");
-        BodyPart htmlPage = new MimeBodyPart();
-        freemarkerMailConfiguration.setClassForTemplateLoading(DefaultEmailSenderImpl.class, "/");
-        Template htmlTemplate = freemarkerMailConfiguration.getTemplate(
-                TEMPLATE_PATH + "/" + tmpl);
-        final StringWriter htmlWriter = new StringWriter();
-        try {
-            htmlTemplate.process(templateTokens, htmlWriter);
-        } catch (TemplateException e) {
-            throw new MailPreparationException("Can't generate HTML mail", e);
-        }
-        htmlPage.setDataHandler(new javax.activation.DataHandler(new javax.activation.DataSource() {
-            public InputStream getInputStream() throws IOException {
-                // return new StringBufferInputStream(htmlWriter
-                // .toString());
-                return new ByteArrayInputStream(textWriter.toString().getBytes(CHARSET));
-            }
-
-            public OutputStream getOutputStream() throws IOException {
-                throw new IOException("Read-only data");
-            }
-
-            public String getContentType() {
-                return "text/html";
-            }
-
-            public String getName() {
-                return "main";
-            }
-        }));
-
-        Map<String, String> messages = new HashMap<>();
-        messages.put("textPart", textWriter.toString());
-        messages.put("htmlPart", htmlWriter.toString());
-        return messages;
     }
 
     @Override
